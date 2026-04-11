8d54a17f7f6127a3592256e7b46f3aa8a8792b51
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2026-04-11 07:24:25 +0200

    more cleanup

--- Scarab/Scarab.cpp
+++ Scarab/Scarab.cpp
@@ -1,37 +1,10 @@
 #include <thread>
-#include "ExternalLibraries/GtkWrappers.h"
+#include "gtk_wrappers.hpp"
 #include "spectrometer.hpp"
 #include "ocean_spectrometer.hpp"
 #include "batch_acquisition.hpp"
 #include "interferometry.hpp"
-bool update_display();
-void handle_run_button();
-void draw_spectrum(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_spectrum_frequency(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_spectra_frequency(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_interference_spectrum(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_interference_spectrum_time(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_interference_phase(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void draw_interference_group_delay(GtkDrawingArea* area, cairo_t* cr, int width, int height, gpointer data);
-void drop_down_change_callback();
-void handle_get_overlay0();
-void handle_get_overlay1();
-void handle_get_overlay2();
-void handle_collapse_panel();
-void handle_delete_overlay0();
-void handle_delete_overlay1();
-void handle_delete_overlay2();
-void handle_get_dark_spectrum();
-void handle_delete_dark_spectrum();
-void handle_refresh_request();
-void save_path_callback();
-void handle_save();
-void handle_reference_A();
-void handle_reference_B();
-void handle_reset_controller();
-void handle_reset_phase();
-void handle_save_phase();
-void svg_callback();
+#include "Scarab.hpp"
 
 std::vector<std::unique_ptr<Spectrometer>> spectrometer_set;
 BatchAcquisition theBatch;
@@ -255,8 +228,6 @@ public:
 
 };
 MainGui theGui;
-
-
 
 void handle_run_button() {
     if (theGui.running_live()) {
@@ -694,7 +665,6 @@ void draw_spectra_frequency(GtkDrawingArea* area, cairo_t* cr, int width, int he
     bool logPlot = false;
     if (theGui.checkboxes[1].isChecked()) {
         logPlot = true;
-
     }
 
     bool forceX = false;
