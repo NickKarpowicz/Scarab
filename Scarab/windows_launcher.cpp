1c875c0f4c8523c81cde647672d1a448d2d45674
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2026-04-23 14:29:21 +0200

    fix windows_launcher (maybe)

--- Scarab/windows_launcher.cpp
+++ Scarab/windows_launcher.cpp
@@ -1,6 +1,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
+#include <shlwapi.h>
+#include <stdio.h>
+#include <tchar.h>
 int wmain(void)
 {
     wchar_t exe_path[MAX_PATH];
@@ -10,15 +12,8 @@ int wmain(void)
     wchar_t bin_path[MAX_PATH];
     swprintf(bin_path, L"%s\\bin", exe_path);
 
-    SetEnvironmentVariableW(L"GTK_EXE_PREFIX", bin_path);
-    SetEnvironmentVariableW(L"GTK_DATA_PREFIX", exe_path);
-    SetEnvironmentVariableW(L"GSETTINGS_SCHEMA_DIR",
-        L"%s\\share\\glib-2.0\\schemas", exe_path);
-
-    SetDllDirectoryW(L"%s\\lib");
-
     wchar_t command_line_call[MAX_PATH * 2];
-    swprintf(command_line_call, L"\"%s\\myapp.exe\"", bin_path);
+    swprintf(command_line_call, L"\"%s\\scarab.exe\"", bin_path);
     STARTUPINFOW si = { sizeof(si) };
     PROCESS_INFORMATION pi;
     CreateProcessW(NULL, command_line_call, NULL, NULL, FALSE, 0,
